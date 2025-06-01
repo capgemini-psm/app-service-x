040aa9f49b4c0842fec642960f0e943b3fcbd6e8
capgemini-psm <paramjot-singh.malhotra@capgemini.com>
2025-06-01 09:12:11 +0530

    added /hello endpoint

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "App Service says Hello!");
 
 app.Run();
